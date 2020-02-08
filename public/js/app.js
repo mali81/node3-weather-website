a6a9044418e9fbaa3ad5eea80bba23710cc07c2c
mali81 <moh_eng_81@yahoo.com>
2020-02-08 20:32:46 +0300

    removing google API-key

--- public/js/app.js
+++ public/js/app.js
@@ -53,49 +53,7 @@ messagetwo.textContent=data.forecastdata
 })
 
 
-    // async function postData(url = '', data = {}) {
-    //     // Default options are marked with *
-    //     const response = await fetch(url, {
-    //       method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    //       mode: 'cors', // no-cors, *cors, same-origin
-    //       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    //       credentials: 'same-origin', // include, *same-origin, omit
-    //       headers: {
-    //         'Content-Type': 'application/json'
-    //         // 'Content-Type': 'application/x-www-form-urlencoded',
-    //       },
-    //       redirect: 'follow', // manual, *follow, error
-    //       referrerPolicy: 'no-referrer', // no-referrer, *client
-    //       body: JSON.stringify(data) // body data type must match "Content-Type" header
-    //     });
-    //     return await response.json(); // parses JSON response into native JavaScript objects
-    //   }
-      
-      
-    //   const celldata ={
-    //   homeMobileCountryCode: 427,
-    //     homeMobileNetworkCode: 02,
-    //     radioType: 'lte',
-    //     carrier: 'Vodafone QA',
-    //     considerIp: 'true',
-    //   cellTowers: [
-    //       {
-    //         cellId: 330,
-    //         locationAreaCode: 161,
-    //         mobileCountryCode: 427,
-    //         mobileNetworkCode: 02,
-    //         age: 10,
-    //         signalStrength: -60,
-    //         timingAdvance: 15
-    //       }
-    //     ]
-    //   }
-      
-      
-    //   postData('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCjgSW-zVx39tXSqpEoiLHeLWf-vO7BEHw', celldata)
-    //     .then((data) => {
-    //       console.log(data); // JSON data parsed by `response.json()` call
-    //     });
+   
       
 
 
